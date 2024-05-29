2a6c853523f04c5c9fe4d51c04f4e2b153f13c01
Martin-38 <martin.lavallee38@gmail.com>
2024-05-29 20:26:47 +0200

    hello

--- src/components/MoviePreview/MoviePreview.tsx
+++ src/components/MoviePreview/MoviePreview.tsx
@@ -1,11 +1,12 @@
+import { Movie } from "../../models";
 import styles from "./MoviePreview.module.css";
 
-const MoviePreview = () => {
-  const goToMovieDetails = () => {
+const MoviePreview = ({movie}: {movie: Movie}) => {
+  const goToMovieDetails = () => {  
     // A DEFINIR EXPERT
   };
 
-  return <div className={styles.movieCard} onClick={goToMovieDetails}></div>;
+  return <div className={styles.movieCard} onClick={goToMovieDetails}> </div>;
 };
 
 export default MoviePreview;
